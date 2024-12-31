1c66e99dea227618d5fda38e16fcfad8c1ccdcaf
ahj30420 <ahj30420@naver.com>
2024-12-31 16:25:34 +0900

    - dfs(프로그래머스 "네트워크")

---
+++ src/algorithm/DFS/Network.java
@@ -0,0 +1,28 @@
+package algorithm.DFS;
+
+// 네트워크
+public class Network {
+    public int solution(int n, int[][] computers) {
+        boolean[] visited = new boolean[n];
+        int count = 0;
+
+        for(int i = 0; i < n; i++){
+            if(!visited[i]){
+                dfs(i, computers, visited);
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    private void dfs(int node, int[][] computers, boolean[] visited){
+        visited[node] = true;
+
+        for(int i = 0; i < computers.length; i++){
+            if(computers[node][i] == 1 && !visited[i]){
+                dfs(i, computers, visited);
+            }
+        }
+    }
+}
