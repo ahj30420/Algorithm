240ae290aadcbb90ce18567431ebd70f6b504866
ahj30420 <ahj30420@naver.com>
2025-01-08 16:40:49 +0900

    - dynamic(백준 "2xn 타일링")

--- src/algorithm/dynamic/Tiliing2XN.java
+++ src/algorithm/dynamic/Tiliing2XN.java
@@ -6,20 +6,19 @@ import java.io.*;
 //2XN 타일링
 public class Tiliing2XN {
     public static void main(String[] args) throws IOException{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        long[] dp = new long[n+1];
+        int N = Integer.parseInt(reader.readLine());
+        long[] dp = new long[N+1];
 
         dp[0] = 1;
         dp[1] = 1;
 
-        for(int i = 2; i <= n; i++){
-            dp[i] = (dp[i-1] + dp[i-2])%10007;
+        for(int i = 2; i <= N; i++) {
+            dp[i] = (dp[i-1] + dp[i-2]) % 10007;
         }
 
-
-        System.out.println(dp[n]);
+        System.out.println(dp[N]);
+        reader.close();
     }
 }
