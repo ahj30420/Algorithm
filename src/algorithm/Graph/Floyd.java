482da5e1a5cb0d214c0147cf93bab66e4b8fce5d
ahj30420 <ahj30420@naver.com>
2025-01-09 13:49:37 +0900

    - Floyd(백준 "플로이드")

---
+++ src/algorithm/Graph/Floyd.java
@@ -0,0 +1,61 @@
+package algorithm.Graph;
+
+import java.io.*;
+import java.util.*;
+
+// 플로이드
+public class Floyd {
+    static final int INF = 100000000;
+    static int n, m;
+    static int[][] dist;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer tokenizer;
+
+        n = Integer.parseInt(reader.readLine());
+        m = Integer.parseInt(reader.readLine());
+        
+        dist = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            Arrays.fill(dist[i], INF);
+            dist[i][i] = 0;
+        }
+
+        for (int i = 0; i < m; i++) {
+            tokenizer = new StringTokenizer(reader.readLine());
+            int u = Integer.parseInt(tokenizer.nextToken());
+            int v = Integer.parseInt(tokenizer.nextToken());
+            int w = Integer.parseInt(tokenizer.nextToken());
+            dist[u][v] = Math.min(dist[u][v], w);
+        }
+
+        floydWarshall();
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (dist[i][j] == INF) {
+                    sb.append(0).append(" ");
+                } else {
+                    sb.append(dist[i][j]).append(" ");
+                }
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    static void floydWarshall() {
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+    }
+}
+
