84cb94a9eb150ef8b4d8114359719f2f447b0d91
HYEONGJUN AHN <79964990+ahj30420@users.noreply.github.com>
2025-10-02 17:29:59 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 11.13 ms, Memory: 84.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+        long answer = 0;
+        int deliverRemain = 0;
+        int pickupRemain = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            deliverRemain += deliveries[i];
+            pickupRemain += pickups[i];
+
+            while (deliverRemain > 0 || pickupRemain > 0) {
+                deliverRemain -= cap;
+                pickupRemain -= cap;
+
+                answer += (long) (i + 1) * 2;
+            }
+        }
+
+        return answer;
+    }
+}
