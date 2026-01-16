08059f1f8fc1810af2597125f7fe452886400c62
HYEONGJUN AHN <79964990+ahj30420@users.noreply.github.com>
2026-01-16 17:33:34 +0900

    [level 1] Title: K번째수, Time: 0.07 ms, Memory: 70.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer = new int[commands.length];
+        for (int i = 0; i < commands.length; i++) {
+            answer[i] = func(array, commands[i]);
+        }
+        return answer;
+    }
+    
+    private int func(int[] array, int[] command) {
+        int i = command[0] - 1;
+        int j = command[1] - 1;
+        int k = command[2] - 1;
+        
+        List<Integer> list = new ArrayList<>();
+        
+        for(int n = i; n <= j; n++) {
+            list.add(array[n]);
+        }
+        
+        Collections.sort(list);
+        
+        return list.get(k);
+    }
+}
