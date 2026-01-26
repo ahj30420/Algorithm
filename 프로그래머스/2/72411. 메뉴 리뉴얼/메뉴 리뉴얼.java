290c85e11ca96249d06998db56e4d79d368b59f4
HYEONGJUN AHN <79964990+ahj30420@users.noreply.github.com>
2026-01-26 17:33:36 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 32.17 ms, Memory: 92.5 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.java
@@ -1,72 +1,54 @@
 import java.util.*;
 
 class Solution {
-    private String[] orderList;
-    private Map<String, Integer> map = new HashMap<>();
-    
+
+    Map<String, Integer> map = new HashMap<>();
+
     public String[] solution(String[] orders, int[] course) {
-        orderList = orders;
-        
-        for (int c : course) {
+
+        for (int i = 0; i < orders.length; i++) {
+            char[] arr = orders[i].toCharArray();
+            Arrays.sort(arr);
+            orders[i] = new String(arr);
+        }
+
+        for (int len : course) {
             for (String order : orders) {
-                String[] str = new String[c];
-                backTracking(ArrayString(order), c, str, 0, 0);
+                backTracking(order, "", 0, 0, len);
             }
         }
-        
+
         List<String> result = new ArrayList<>();
-        Set<String> menuCombination = map.keySet();
-        for (int c : course) {
-            int maxCount = 0;
-            for (String menu : menuCombination) {
-                if (menu.length() == c) {
-                    maxCount = Math.max(maxCount, map.get(menu));
+
+        for (int len : course) {
+            int max = 2;
+
+            for (String key : map.keySet()) {
+                if (key.length() == len) {
+                    max = Math.max(max, map.get(key));
                 }
             }
 
-            for (String menu : menuCombination) {
-                if (menu.length() == c && map.get(menu) == maxCount) {
-                    result.add(menu);
+            for (String key : map.keySet()) {
+                if (key.length() == len && map.get(key) == max) {
+                    result.add(key);
                 }
             }
         }
-        
+
         Collections.sort(result);
-        
+
         return result.toArray(new String[0]);
     }
-    
-    private String ArrayString(String order) {
-        char[] chars = order.toCharArray();
-        Arrays.sort(chars);
-        return new String(chars);
-    }
-    
-    private void backTracking(String order, int course, String[] str, int preIndex, int currentDepth) {
-        if (currentDepth == course) {
-            
-            String combination = String.join("", str);
-            int count = 0;
-            for (String targetOrder : orderList) {
-                boolean hasAll = true;
-                for (String s : str) {
-                    if (!targetOrder.contains(s)) {
-                        hasAll = false;
-                        break;
-                    }
-                }    
-                if (hasAll) count++;
-            }
-            
-            if (count >= 2) {
-                map.put(combination, count);
-            }
+
+    private void backTracking(String order, String comb, int idx, int depth, int target) {
+        if (depth == target) {
+            map.put(comb, map.getOrDefault(comb, 0) + 1);
             return;
         }
-        
-        for (int i = preIndex; i < order.length(); i++) {
-            str[currentDepth] = Character.toString(order.charAt(i));
-            backTracking(order, course, str, i+1, currentDepth+1);
+
+        for (int i = idx; i < order.length(); i++) {
+            backTracking(order, comb + order.charAt(i), i + 1, depth + 1, target);
         }
     }
 }
